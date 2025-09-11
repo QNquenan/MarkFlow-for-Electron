87ad3c144344fd36ae456ca999d1c7d2584ff583
QNquenan <qn2987271942@outlook.com>
2025-09-11 22:19:36 +0800

    style: 🎨 修改注释

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,12 +1,11 @@
 import { contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
-// Custom APIs for renderer
+// 为渲染进程提供的自定义API
 const api = {}
 
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
+// 仅当启用上下文隔离时，使用 `contextBridge` API
+// 将Electron API暴露给渲染进程，否则直接添加到DOM全局对象中
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI)
