2b32ea0a29fed082e3566a2548fecff324e1e1b8
QNquenan <qn2987271942@outlook.com>
2025-09-11 21:53:07 +0800

    feat: ✨ 添加 ico

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,7 @@
 import { app, shell, BrowserWindow, ipcMain } from 'electron'
-import { join } from 'path'
+import path, { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/favicon.ico'
+// import icon from '../../resources/favicon.png'
 
 function createWindow() {
   // 创建浏览器窗口
@@ -11,7 +11,7 @@ function createWindow() {
     title: 'MarkFlow',
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
+    icon: path.join(__dirname, '../../resources/favicon.png'),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false
