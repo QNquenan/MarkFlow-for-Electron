e5dfbeaadac139e73e8a8ba46a817f68ed636b67
QNquenan <qn2987271942@outlook.com>
2025-09-12 20:19:07 +0800

    feat: ✨ 新增fluent布局样式

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
 import HeaderBar from './views/HeaderBar.vue'
+import Sidebar from './views/Sidebar.vue'
 </script>
 
 <template>
-  <div>
-    <HeaderBar />
+  <div class="layout">
+    <Sidebar />
+    <div class="rightBox">
+      <HeaderBar />
+      <div class="main"></div>
+    </div>
   </div>
 </template>
