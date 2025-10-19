3fc7078e46c5a599e02561b35e19c84a75c891de
QNquenan <qn2987271942@outlook.com>
2025-10-19 12:15:24 +0800

    fix: 🐛 修复生产环境下首页白屏的问题

--- src/renderer/src/router/index.js
+++ src/renderer/src/router/index.js
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
+// import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   {
@@ -19,7 +20,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
